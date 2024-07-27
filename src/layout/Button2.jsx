1976b28e29df311cc60d4ff4431340daf9839882
Serhii-1979 <krasovskyi76@gmail.com>
2024-07-27 12:55:06 +0200

    add new file

--- src/layout/Button2.jsx
+++ src/layout/Button2.jsx
@@ -3,7 +3,7 @@ import styles from "./Button2.module.css"
 function Button2() {
     return(
         <div className={styles.button_cont}>
-            <button className={styles.button_Add}>Continue Shopping</button>
+            <button className={styles.button_Add}>Add to card</button>
         </div>
     )
 }
